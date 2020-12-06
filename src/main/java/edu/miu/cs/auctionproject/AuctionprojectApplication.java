f905cc7f59add7cbd4354ea472f379c904b0d600
Biniam Arefaine <binireyes@gmail.com>
2020-12-06 13:27:05 -0600

    project created

---
+++ src/main/java/edu/miu/cs/auctionproject/AuctionprojectApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.auctionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/auctionproject/AuctionprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.auctionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
