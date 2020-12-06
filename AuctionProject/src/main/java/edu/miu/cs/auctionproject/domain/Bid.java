60a19e41aeadedf256ef6069ea674ca76621834c
etezare <jessetezare@gmail.com>
2020-12-06 15:11:24 -0600

    class added

--- AuctionProject/src/main/java/edu/miu/cs/auctionproject/domain/Bid.java
+++ AuctionProject/src/main/java/edu/miu/cs/auctionproject/domain/Bid.java
@@ -1,4 +1,30 @@
 package edu.miu.cs.auctionproject.domain;
 
+import jdk.vm.ci.meta.Local;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Data
 public class Bid {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private LocalDate dueDate;
+
+    @OneToOne
+    @Column(nullable = false)
+    private Product product;
+
+
+
+
+
+
 }
--- AuctionProject/src/main/java/edu/miu/cs/auctionproject/domain/User.java
+++ AuctionProject/src/main/java/edu/miu/cs/auctionproject/domain/User.java
@@ -1,4 +1,23 @@
 package edu.miu.cs.auctionproject.domain;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String licenceNumber;
+    @OneToMany
+    private List<Product> product;
+    private boolean verification;
+
+
+
+
+
 }
