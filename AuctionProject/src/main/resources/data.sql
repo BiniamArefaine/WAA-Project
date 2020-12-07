e17cb547cfba5b20b12a2c23afab7047386f4119
eyob05 <65434701+eyob05@users.noreply.github.com>
2020-12-07 15:55:47 -0600

    static data

--- AuctionProject/src/main/resources/data.sql
+++ AuctionProject/src/main/resources/data.sql
@@ -2,3 +2,42 @@ SET foreign_key_checks = 0;
 insert into bid values(1,'2020-03-02',1);
 insert into bid values(2,'2020-05-02',2);
 insert into bid values(3,'2019-05-02',3);
+
+SET foreign_key_checks = 0;
+insert into product values(1,'perfect product','car',0,0,20000,1);
+insert into product values(2,'it is great','phone',0,0,900,2);
+insert into product values(3,'what a nice','laptop',0,0,2000,3);
+
+SET foreign_key_checks = 0;
+insert into user values(1,'eyob@gmail.com','Eyob','Habtom','ey0123',0,1);
+insert into user values(2,'bini@gmail.com','Bini','Tshaye','bi0234',0,1);
+insert into user values(3,'dawit@gmail.com','Dawit','tesfahanes','da0567',0,1);
+insert into user values(4,'essey@gmail.com','Essey','Abreham','es0789',0,2);
+insert into user values(5,'wegahta@gmail.com','Wegahta','Gerbremariam','we0213',0,2);
+
+SET foreign_key_checks = 0;
+insert into role values(1,'SELLER');
+insert into role values(2,'CUSTOMER');
+
+SET foreign_key_checks = 0;
+insert into category values(1,'ELECTRONICS');
+insert into category values(2,'CARS');
+
+SET foreign_key_checks = 0;
+insert into product_categories values(1,2);
+insert into product_categories values(2,1);
+insert into product_categories values(3,1);
+
+SET foreign_key_checks = 0;
+insert into bid_user values(1,300,1);
+insert into bid_user values(2,10000,2);
+
+SET foreign_key_checks = 0;
+insert into deposit_payment values(1,90,2,4);
+insert into deposit_payment values(2,200,3,5);
+
+SET foreign_key_checks = 0;
+insert into product_image_link values(1,'main/resource/images');
+insert into product_image_link values(2,'main/resource/images');
+
+
