76e6c80812846efa066397c60fc5b92dfbb850c2
Biniam Arefaine <binireyes@gmail.com>
2020-12-06 22:11:10 -0600

    modified

--- AuctionProject/src/main/java/edu/miu/cs/auctionproject/controller/CategoryController.java
+++ AuctionProject/src/main/java/edu/miu/cs/auctionproject/controller/CategoryController.java
@@ -1,7 +1,46 @@
 package edu.miu.cs.auctionproject.controller;
 
+import edu.miu.cs.auctionproject.domain.Bid;
+import edu.miu.cs.auctionproject.domain.Category;
+import edu.miu.cs.auctionproject.service.BidService;
+import edu.miu.cs.auctionproject.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+
+    @GetMapping("/getall_categories")
+    private String getAllBids(){
+        List<Category> allCategories = categoryService.getAllCategories();
+        System.out.println(allCategories + "---------------");
+        return "Home";
+    }
+
+    @PostMapping("/save_categories")
+    private void saveCategory(Category category){
+        categoryService.save(category);
+    }
+
+    @GetMapping("/get_categoriesById")
+    private String getCategoriesById(Long id){
+        Optional<Category> category = categoryService.getCategoryById(id);
+        System.out.println(category + "---------------");
+        return "Home";
+    }
+    @DeleteMapping("/delete_categroy")
+    private void deleteCategory(Long id){
+        categoryService.deleteCategoryById(id);
+    }
 }
--- AuctionProject/src/main/java/edu/miu/cs/auctionproject/repository/BidRepository.java
+++ AuctionProject/src/main/java/edu/miu/cs/auctionproject/repository/BidRepository.java
@@ -2,8 +2,10 @@ package edu.miu.cs.auctionproject.repository;
 
 import edu.miu.cs.auctionproject.domain.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BidRepository extends JpaRepository<Bid, Long> {
+
 }
