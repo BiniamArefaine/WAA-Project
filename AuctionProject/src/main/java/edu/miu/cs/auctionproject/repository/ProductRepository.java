8f4eee1dc98561b7a596cc45f570931ee45d93fa
etezare <jessetezare@gmail.com>
2020-12-09 17:18:46 -0600

    committed

--- AuctionProject/src/main/java/edu/miu/cs/auctionproject/repository/ProductRepository.java
+++ AuctionProject/src/main/java/edu/miu/cs/auctionproject/repository/ProductRepository.java
@@ -17,10 +17,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 //    List<Product> findAllByStartingPriceOrderByUpLoadedDateDesc();
 //    @Query(value = "SELECT p FROM  Product p WHERE p.productName like %:searchString%")
 
-    @Query(value = "SELECT p FROM Product p WHERE p.release='yes'" )
+    @Query(value = "SELECT p FROM Product p WHERE p.release='Yes'" )
      Page<Product> findAllReleased(Pageable var1);
 
-    @Query("select p from Product p where p.productName like %:searchString% and p.release='yes'")
+    @Query("select p from Product p where p.productName like %:searchString% and p.release='Yes'")
     Page<Product> findByProductName(Pageable var1,String searchString);
 
 }
